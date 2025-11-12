5231401ccb161c9d1245fa0bf687b6fc60cf113d
Ali Abdullah <aliabdullah1215@icloud.com>
2025-11-12 18:27:08 +0000

    Update Task 1

--- loop.c
+++ loop.c
@@ -1,73 +1,58 @@
 #include "shell.h"
 
 /**
- * trim_input - Removes leading and trailing spaces or tabs from a line
- * @line: Input string to be trimmed
- *
- * Return: Pointer to the first non-space character
+ * main - simple UNIX command interpreter
+ * Return: Always 0
  */
-char *trim_input(char *line)
+int main(void)
 {
-	int i = 0, end;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    pid_t pid;
+    int status;
+    char *argv[2];
 
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+            write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-	line += i;
-	end = strlen(line) - 1;
+        nread = getline(&line, &len, stdin);
+        if (nread == -1) /* EOF (Ctrl+D) */
+        {
+            if (isatty(STDIN_FILENO))
+                write(STDOUT_FILENO, "\n", 1);
+            break;
+        }
 
-	while (end >= 0 && (line[end] == ' ' || line[end] == '\t'))
-	{
-		line[end] = '\0';
-		end--;
-	}
+        /* إزالة \n */
+        if (line[nread - 1] == '\n')
+            line[nread - 1] = '\0';
 
-	return (line);
-}
+        if (line[0] == '\0')
+            continue;
 
-/**
- * shell_loop - Main loop of the shell
- * @argv: Program name (used for error messages)
- *
- * Return: Last command status
- */
-int shell_loop(char **argv)
-{
-	char *line = NULL, *cmd;
-	size_t len = 0;
-	ssize_t nread;
-	int cmd_count = 0, last_status = 0;
+        argv[0] = line;
+        argv[1] = NULL;
 
-	(void)argv;
-
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "#cisfun$ ", 9);
-
-		nread = getline(&line, &len, stdin);
-		if (nread == -1)
-		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			break;
-		}
-
-		if (nread > 0 && line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
-
-		cmd = trim_input(line);
-		if (cmd[0] == '\0')
-			continue;
-
-		if (handle_builtin(cmd))
-			continue;
-
-		cmd_count++;
-		last_status = execute_command(cmd, argv, cmd_count);
-	}
-
-	free(line);
-	return (last_status);
+        pid = fork();
+        if (pid == 0)
+        {
+            if (execve(argv[0], argv, environ) == -1)
+                perror("./shell");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid < 0)
+        {
+            perror("fork");
+        }
+        else
+        {
+            waitpid(pid, &status, 0);
+        }
+    }
+    free(line);
+    return (0);
 }
 
--- shell.h
+++ shell.h
@@ -4,15 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
 
 extern char **environ;
 
-int shell_loop(char **argv);
-int execute_command(char *line, char **argv, int cmd_count);
-int handle_builtin(char *cmd);
-
 #endif /* SHELL_H */
-
